41b3ff07b554c530c880bf6cb3dbcd7c4b254a3d
Peh4 <peh4@macbook-air-de-peha.home>
2016-02-06 17:00:24 +0100

    few details

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
 $bcode 		= $_GET['BCODE'];
-$scanType 	= $_GET['SCAN_TYPE']
-$scanMeta 	= $_GET['SCAN_META']
-$scanRaw 	= $_GET['SCAN_RAW']
+$scanType 	= $_GET['SCAN_TYPE'];
+$scanMeta 	= $_GET['SCAN_META'];
+$scanRaw 	= $_GET['SCAN_RAW'];
 $bcodes 	= Array();	
 array_push($bcodes,$bcode);
-$goScan 	= '"zxing://scan/?ret='.urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?BCODE={CODE}&SCAN_TYPE={TYPE}&SCAN_META={META}&SCAN_RAW={RAWCODE}').'&SCAN_FORMATS={FORMAT}"'
+$goScan 	= '"zxing://scan/?ret='.urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?BCODE={CODE}&SCAN_TYPE={TYPE}&SCAN_META={META}&SCAN_RAW={RAWCODE}').'&SCAN_FORMATS={FORMAT}"';
 
 ?>
 
@@ -18,7 +18,7 @@ $goScan 	= '"zxing://scan/?ret='.urlencode('http://'.$_SERVER['HTTP_HOST'].$_SER
 </head>
 <body>
 
-Intallez <a href="https://play.google.com/store/apps/details?id=com.google.zxing.client.android&hl=fr" Barcode Scanner</a> sur votre téléphone Android 
+Intallez <a href="https://play.google.com/store/apps/details?id=com.google.zxing.client.android&hl=fr"> Barcode Scanner</a> sur votre téléphone Android 
 Puis ouvrez cette page depuis votre téléphone, vous serez re-dirigé vers l'application.
 Scannez un article, vous serez re-dirigez sur cette page, les paramètres du code barre sont alors récupérées, puis vous serez à nouveau re-dirigé vers l'application pour scanner un nouveau code-barre.
 
