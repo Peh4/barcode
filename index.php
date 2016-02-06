bf37102709fa7ae3fae95ba470a619a41da7287a
Peh4 <peh4@macbook-air-de-peha.home>
2016-02-06 15:40:45 +0100

    Barcode scan loop

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$bcode = $_GET['bcode'];
+$bcodes = Array();	
+array_push($bcodes,$bcode);
+$goScan = '"zxing://scan/?ret='.urlencode('http://peh4.com/barcode/?bcode={CODE}').'&SCAN_FORMATS=UPC_A,EAN_13"'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TestBarcode</title>
+</head>
+<body>
+<?php print_r ($bcodes); ?>
+	<script>
+		window.location.href = <?php echo $goScan ?>;
+	</script>
+</body>
+</html>
