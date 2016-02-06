fc09043b22c8199c4dd2013a448b45bc930e3870
Peh4 <peh4@macbook-air-de-peha.home>
2016-02-06 16:09:31 +0100

    Ajout de quelques explications sur le fonctionnement

--- index.php
+++ index.php
@@ -14,6 +14,12 @@ $goScan = '"zxing://scan/?ret='.urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERV
 	<title>TestBarcode</title>
 </head>
 <body>
+
+Intallez <a href="https://play.google.com/store/apps/details?id=com.google.zxing.client.android&hl=fr" Barcode Scanner</a> sur votre téléphone Android 
+Puis ouvrez cette page depuis votre téléphone, vous serez re-dirigé vers l'application.
+Scannez un article, vous serez re-dirigez sur cette page, les paramètres du code barre sont alors récupérées, puis vous serez à nouveau re-dirigé vers l'application pour scanner un nouveau code-barre.
+
+
 <?php print_r ($bcodes); ?>
 	<script>
 		window.location.href = <?php echo $goScan ?>;
